dfa9efca744b3907c65f120da11716de1bbd9bff
doquocdung20021315 <doquocdung262002@gmail.com>
2024-05-19 10:11:13 +0700

    config

--- src/config/db.js
+++ src/config/db.js
@@ -1,9 +1,27 @@
-require('dotenv').config();
+require("dotenv").config();
 const mongoose = require("mongoose");
 
 const connect = mongoose
-  .connect(`${process.env.MONGODB_URI}/${process.env.MONGODB_DATABASE}`)
-  .then(() => {}) // console.log("Đã kết nối với MongoDB")
+  .connect(`${process.env.MONGODB_URI}${process.env.MONGODB_DATABASE}`)
+  .then(() => {
+    console.log("Đã kết nối với MongoDB");
+
+    // Gọi phương thức disconnect() khi nhận tín hiệu ngắt (Ctrl+C)
+    process.on("SIGINT", async () => {
+      try {
+        await mongoose.disconnect();
+        process.exit(0); // Thoát ứng dụng sau khi đóng kết nối
+      } catch (error) {
+        console.error("Lỗi khi đóng kết nối:", error);
+        process.exit(1); // Thoát ứng dụng với lỗi
+      }
+    });
+
+    // Gọi phương thức disconnect() khi ứng dụng kết thúc
+    process.on("exit", () => {
+      mongoose.disconnect();
+    });
+  }) // console.log("Đã kết nối với MongoDB")
   .catch((error) => console.log("Lỗi kết nối MongoDB:", error));
 
 module.exports = connect;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 const express = require("express");
-const morgan = require('morgan');
-const cors = require('cors');
-require('dotenv').config();
-const connectDB = require('./config/db');
-const accountRouter = require('./routes/account');
-const doctorRouter = require('./routes/doctor');
-const specialistRouter = require('./routes/specialist');
-const facilityRouter = require('./routes/facility');
-const appointmentRouter = require('./routes/appointment');
+const morgan = require("morgan");
+const cors = require("cors");
+require("dotenv").config();
+const connectDB = require("./config/db");
+const accountRouter = require("./routes/account");
+const doctorRouter = require("./routes/doctor");
+const specialistRouter = require("./routes/specialist");
+const facilityRouter = require("./routes/facility");
+const appointmentRouter = require("./routes/appointment");
 
 const app = express();
-const hostname = process.env.APP_HOST;
-const port = process.env.APP_PORT;
+const hostname = process.env.LOCAL_DEV_APP_HOST;
+const port = process.env.LOCAL_DEV_APP_PORT;
 
 connectDB;
 
@@ -20,18 +20,24 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use('/account', accountRouter);
-app.use('/doctor', doctorRouter);
-app.use('/specialist', specialistRouter);
-app.use('/facility', facilityRouter);
-app.use('/appointment', appointmentRouter);
+app.use("/account", accountRouter);
+app.use("/doctor", doctorRouter);
+app.use("/specialist", specialistRouter);
+app.use("/facility", facilityRouter);
+app.use("/appointment", appointmentRouter);
 
 app.get("/", (req, res) => {
   res.send("API");
 });
 
-app.listen(port, hostname, () => {
-  console.log(`Appointment Booking App listening at ${hostname}:${port}`);
-});
+if (process.env.BUILD_MODE === "production") {
+  app.listen(process.env.PORT, () => {
+    console.log(`Appointment Booking App listening at Port: ${process.env.PORT}`);
+  });
+} else {
+  app.listen(port, hostname, () => {
+    console.log(`Appointment Booking App listening at ${hostname}:${port}`);
+  });
+}
 
 module.exports = app;
